f91a48eef36a3dee3ca12b1ccf9cc3813effe5e7
Devis Battisti <devis.battisti@uqido.com>
2020-11-11 10:05:44 +0100

    volume

--- docker.db.run.sh
+++ docker.db.run.sh
@@ -6,6 +6,6 @@ docker run \
   -e POSTGRES_PASSWORD=$DATABASE_PASSWORD \
   -e POSTGRES_DB=$DATABASE_NAME \
   -p $DATABASE_PORT:5432 \
-  -v ~/code/data/$PROJECT_NAME/pg:/var/lib/postgresql/data \
+  -v $PROJECT_NAME-pgsql:/var/lib/postgresql/data \
   -d \
   postgres:12
